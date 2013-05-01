acd21ef582f23f6761b88d1142556e81a9bbe9ad
Brian C <brian.m.carlson@gmail.com>
2013-04-30 21:33:37 -0500

    Update elastic-search.sh

--- elastic-search.sh
+++ elastic-search.sh
@@ -21,3 +21,12 @@ tar xzvf elasticsearch-0.20.4.tar.gz
 wget http://download.graylog2.org/graylog2-server/graylog2-server-0.11.0.tar.gz
 tar xzvf graylog2-server-0.11.0.tar.gz
 # TODO - download config files
+
+#install mongodb
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/10gen.list
+apt-get update
+apt-get install mongodb-10gen=2.2.3
+echo "mongodb-10gen hold" | dpkg --set-selections
+
+#http://thetek.net/how-to/13-how-to/42-how-to-install-graylog2-on-ubuntu.html
